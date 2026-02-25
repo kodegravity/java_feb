b799c7a82a6761742a8f73c34ce67dbc4cf89dc8
Sunny Singh <sunny@Sunnys-Laptop.local>
2026-02-24 21:39:56 -0500

    constructor

--- src/feb24/DateDemo.java
+++ src/feb24/DateDemo.java
@@ -6,25 +6,22 @@ public class DateDemo {
     int year;
 
     public DateDemo() {
-        System.out.println("Default constructor");
+        System.out.println("Default constructor");//1
     }
-
     public DateDemo(int day) {
         this();
-        System.out.println("Constructor with 1 parameter");
+        System.out.println("Constructor with 1 parameter");//2
         this.day = day;
     }
-
     public DateDemo(int day, int month) {
         this(10);
-        System.out.println("Constructor with 2 parameter");
+        System.out.println("Constructor with 2 parameter");//3
         this.day = day;
         this.month = month;
     }
-
     public DateDemo(int day, int month, int year) {
         this(10,22);
-        System.out.println("Constructor with 3 parameter");
+        System.out.println("Constructor with 3 parameter");//4
         this.day = day;
         this.month = month;
         this.year = year;
